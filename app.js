66410ae724227cd220e76c8c8d65ffa8f88efde5
Gabriel Delattre <gabriel@soixantecircuits.fr>
2013-05-09 00:58:56 +0200

    adding all modules and first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// Require
+var watchr = require('watchr');
+
+// Watch a directory or file
+console.log('Watch our paths');
+watchr.watch({
+    paths: ['./tmp'],
+    listeners: {
+        log: function(logLevel){
+            console.log('a log message occured:', arguments);
+        },
+        error: function(err){
+            console.log('an error occured:', err);
+        },
+        watching: function(err,watcherInstance,isWatching){
+            if (err) {
+                console.log("watching the path " + watcherInstance.path + " failed with error", err);
+            } else {
+                console.log("watching the path " + watcherInstance.path + " completed");
+            }
+        },
+        change: function(changeType,filePath,fileCurrentStat,filePreviousStat){
+            console.log('a change event occured:',arguments);
+        }
+    }
+});
+
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
